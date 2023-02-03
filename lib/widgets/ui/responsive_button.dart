5ef0f69f7183d06dc26ca250c90cde0b43684da9
Chhayyy <huotchhayyy@gmail.com>
2023-02-03 23:37:50 +0700

    completed reusable button

--- lib/widgets/ui/responsive_button.dart
+++ lib/widgets/ui/responsive_button.dart
@@ -1,29 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ResponsiveButton extends StatelessWidget {
-  bool? isResponsive;
+  bool? isIcon;
   double? width;
+  bool? isPrimary;
   final String text;
 
   ResponsiveButton(
-      {super.key, this.width, this.isResponsive = false, required this.text});
+      {super.key,
+      this.width,
+      this.isIcon = false,
+      required this.text,
+      this.isPrimary = false});
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
         child: Container(
       height: 60,
+      width: isIcon == true ? double.maxFinite : width,
+      padding: const EdgeInsets.only(left: 20, right: 20),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16), color: Colors.blue),
+          border: isPrimary == true
+              ? null
+              : Border.all(width: 2, color: CupertinoColors.systemGrey2),
+          borderRadius: BorderRadius.circular(16),
+          color: isPrimary == true ? Colors.blue : Colors.white),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          isResponsive == true
-              ? Text(
-                  text,
-                  style: const TextStyle(color: Colors.white),
-                )
-              : Container(),
+          isIcon == true ? SvgPicture.asset('assets/google.svg') : Container(),
+          const SizedBox(width: 20),
+          Text(
+            text,
+            style: const TextStyle(
+                color: CupertinoColors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w500),
+          )
         ],
       ),
     ));
